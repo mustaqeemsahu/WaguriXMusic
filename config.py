0e34889e5d8d5840a14aa3c675f37f904cc601a4
Mohammed Mustaqeem <mustaqeemsahu@gmail.com>
2026-04-28 15:28:10 +0530

    Update START_IMG URL in config.py

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ class Config:
         self.PING_IMG: str = getenv(
             "PING_IMG", "https://files.catbox.moe/43u332.jpg")    # Ping command image
         self.START_IMG: str = getenv(
-            "START_IMG", "https://files.catbox.moe/43u332.jpg")  # Start command image
+            "START_IMG", "https://files.catbox.moe/ichom4.jpg")  # Start command image
         self.RADIO_IMG: str = getenv(
             "RADIO_IMG", "https://files.catbox.moe/43u332.jpg")    # Radio command image
 
